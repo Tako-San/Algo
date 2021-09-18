09f68c141c23dc014195c942e2bb284e989c2901
Tako-San <dxe13@mail.ru>
2021-09-18 22:50:29 +0300

    Task B done

--- 2_lsn/B.cc
+++ 2_lsn/B.cc
@@ -1,12 +1,23 @@
 #include <iostream>
 
-char get_sym_by_num(size_t strnum, size_t pos) {
+char get_sym(size_t strnum, size_t pos) {
   if (0 == pos)
     return 'a' + strnum;
 
-  // size_t substr_len = 
-
-
+  size_t sublen = 1ull << strnum;
+  size_t new_pos = (pos < sublen) ? (pos - 1) : (pos - sublen);
+  return get_sym(strnum - 1, new_pos);
 }
 
-int main() { return 0; }
+int main() {
+  size_t strnum = 0,
+         beg = 0,
+         end = 0;
+
+  std::cin >> strnum >> beg >> end;
+  for (size_t i = beg; i <= end; ++i )
+    std::cout << get_sym(strnum, i);
+
+  std::cout << std::endl;
+  return 0;
+}
