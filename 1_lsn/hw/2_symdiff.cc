bce338a2f9b113b0877c3ea147632646f7b12b69
Tako-San <dxe13@mail.ru>
2021-09-21 02:35:00 +0300

    symdiff done

--- 1_lsn/hw/2_symdiff.cc
+++ 1_lsn/hw/2_symdiff.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
 namespace Tako
@@ -39,13 +40,62 @@ class Set
 
     bool in(size_t elnum)
     {
-        return (data_[elnum / CHUNK_SIZE] >> (elnum / CHUNK_SIZE)) & 1;
+        return (data_[elnum / CHUNK_SIZE] >> (elnum % CHUNK_SIZE)) & 1;
+    }
+
+    const Set &operator^=(const Set &rhs)
+    {
+        if (max_elem_ != rhs.max_elem_)
+            throw std::runtime_error{"Tako::Set::operator^=: max_elem_ != rhs.max_elem_"};
+
+        for (size_t i = 0; i < max_elem_; ++i)
+            data_[i] ^= rhs.data_[i];
+
+        return *this;
+    }
+
+    void print()
+    {
+        for (size_t i = 1; i < max_elem_; ++i)
+            if (in(i))
+                std::cout << i << " ";
+
+        std::cout << std::endl;
     }
 };
+
+Set operator^(const Set &lhs, const Set &rhs)
+{
+    auto lhs_cpy = lhs;
+    return lhs_cpy ^= rhs;
+}
+
+Set read_set(size_t max_elem)
+{
+    Set res{max_elem};
+
+    Set::ChunkT elem = 0;
+    std::cin >> elem;
+
+    while (elem != 0)
+    {
+        res.insert(elem);
+        std::cin >> elem;
+    }
+
+    return res;
+}
 
 } // namespace Tako
 
 int main()
 {
+    constexpr size_t max_elem = 20000;
+
+    auto s1 = Tako::read_set(max_elem);
+    auto s2 = Tako::read_set(max_elem);
+
+    auto sres = s1 ^ s2;
+    sres.print();
     return 0;
 }
