801af589473262bc4052e87684401b1707ec689d
Tako-San <dxe13@mail.ru>
2022-09-22 20:12:49 +0300

    2 heaps updated for less memory consumption

--- 2022/cw1/1T/1_two_heaps.cc
+++ 2022/cw1/1T/1_two_heaps.cc
@@ -12,26 +12,32 @@ MITM
 */
 
 #include <algorithm>
+#include <concepts>
 #include <iostream>
 #include <limits>
+#include <span>
 #include <vector>
 
-using T = uint64_t;
+using T = uint32_t;
 
-std::pair<T, std::vector<int>> readW(unsigned N)
+template <std::unsigned_integral U> U pow2(size_t pow)
+{
+  return static_cast<U>(1) << pow;
+}
+
+T readW(unsigned N, std::span<int> W)
 {
   T total = 0;
-  std::vector<int> W(N);
   for (unsigned i = 0; i < N; i++)
   {
     std::cin >> W[i];
     total += W[i];
   }
 
-  return {total, std::move(W)};
+  return total;
 }
 
-T calcHeap(const std::vector<int> &W, T set)
+T calcHeap(std::span<int> W, T set)
 {
   auto N = W.size();
   T heap = 0;
@@ -41,27 +47,24 @@ T calcHeap(const std::vector<int> &W, T set)
   return heap;
 }
 
-std::vector<T> getSums(const std::vector<int> &W, T total)
+void getSums(std::span<int> W, T total, std::span<T> sums)
 {
   auto N = W.size();
   /* 2 ^ (N - 1), because we may calc only half */
-  auto maxg = static_cast<T>(1) << (N - 1);
+  auto maxg = pow2<T>(N - 1);
 
-  std::vector<T> sums{};
-  sums.reserve(N);
   for (T g = 0; g < maxg; g++)
   {
     auto heap = calcHeap(W, g);
-    sums.push_back(heap);
-    sums.push_back(total - heap);
+    sums[2 * g] = heap;
+    sums[2 * g + 1] = total - heap;
   }
 
   std::sort(sums.begin(), sums.end());
-  return sums;
 }
 
 /* MITM - Meet In The Middle */
-T mitm(const std::vector<T> &s1, const std::vector<T> &s2, T total)
+T mitm(std::span<T> s1, std::span<T> s2, T total)
 {
   /* from small to large */
   auto cur1 = s1.begin();
@@ -100,15 +103,26 @@ int main()
 {
   unsigned N = 0;
   std::cin >> N;
-
   auto N1 = N / 2;
-  auto [total1, W1] = readW(N1);
-
   auto N2 = N - N1;
-  auto [total2, W2] = readW(N2);
 
-  auto sums1 = getSums(W1, total1);
-  auto sums2 = getSums(W2, total2);
+  std::vector<int> W(N);
+  std::span W1{W.begin(), W.begin() + N1};
+  std::span W2{W.begin() + N1, W.end()};
+
+  auto total1 = readW(N1, W1);
+  auto total2 = readW(N2, W2);
+
+  auto twoPowN1 = pow2<size_t>(N1);
+  auto twoPowN2 = pow2<size_t>(N2);
+  std::vector<T> sums(twoPowN1 + twoPowN2);
+
+  std::span sums1{sums.begin(), sums.begin() + twoPowN1};
+  std::span sums2{sums.begin() + twoPowN1, sums.end()};
+
+  getSums(W1, total1, sums1);
+  getSums(W2, total2, sums2);
 
   std::cout << mitm(sums1, sums2, total1 + total2) << std::endl;
+  return 0;
 }
